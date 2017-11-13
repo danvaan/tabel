6545ef89e99891009910c6ebdd349b31661dfa23
danvaan <danvaan@users.noreply.github.com>
2017-11-13 14:19:14 +0200

    tabel

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int arv = 0;
+
+            /* for (int i = 0; i < 7; i++)
+              {
+                 for (int j = 0; j < 7; j++)
+                     if (i == 0)
+                     {
+                         Console.Write
+                     }
+
+
+
+
+              }
+              */
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write("{0}\t", i);
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.Write("{0}\t", i * j);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
